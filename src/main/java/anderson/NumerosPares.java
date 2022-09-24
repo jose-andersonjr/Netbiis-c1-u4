995a4ecd7428ea82cb93fb38f8c7c312235552e6
José Anderson <jose.andersonjr2@gmail.com>
2022-09-24 16:06:14 -0300

    Edit nem NumerosPares

--- src/main/java/anderson/NumerosPares.java
+++ src/main/java/anderson/NumerosPares.java
@@ -6,16 +6,27 @@ public class NumerosPares {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Favor informar um número: ");
+        System.out.println("Favor informar o número a: ");
         var a = teclado.nextInt();
-
-        System.out.println("Favor informar um número: ");
+        System.out.println("Favor informar o número b: ");
         var b = teclado.nextInt();
-
-        for (int i=a+1;i<b;i++){
-            if (i%2==0) {
-                System.out.println("O número: " +i+ " é par!");
+        if ((a>0) && (b>0)) {
+            if (a>b) {
+                for (int i=b+1;i<a;i++){
+                    if (i%2==0) {
+                        System.out.println("O número: " +i+ " é par!");
+                    }
+                }
+            }else{
+                for (int i=a+1;i<b;i++){
+                    if (i%2==0) {
+                        System.out.println("O número: " +i+ " é par!");
+                    }
+                }
             }
+        }else{
+            System.out.println("Por favor insira a e b maiores que 0");
+            NumerosPares.main(null);
         }
     }
 }
