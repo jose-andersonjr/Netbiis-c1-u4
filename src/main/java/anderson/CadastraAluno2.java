313b9e67a704bc98fb6eec1986778ca4f9d85681
José Anderson <jose.andersonjr2@gmail.com>
2022-09-24 14:43:45 -0300

    Add exercicio CadastraAluno2

---
+++ src/main/java/anderson/CadastraAluno2.java
@@ -0,0 +1,26 @@
+package anderson;
+
+import java.util.Scanner;
+
+public class CadastraAluno2 {
+    public static void main(String[] args) {
+        String nome, endereco, telefone;
+        int matricula,cont;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Favor informar a matrícula do próximo aluno: ");
+        matricula = teclado.nextInt();
+
+        do {
+            if (matricula!=0) {
+                System.out.println("Favor informar o nome ");
+                nome = teclado.next();
+                System.out.println("Favor informar o endereco ");
+                endereco = teclado.next();
+                System.out.println("Favor informar o telefone do aluno: ");
+                telefone = teclado.next();
+                System.out.println("Favor informar a matrícula do próximo aluno: ");
+                matricula = teclado.nextInt();
+            }
+        } while (matricula != 0);
+    }
+}
