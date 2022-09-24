62c0dfa1320cbc96a77e110d5b31594745eae93e
José Anderson <jose.andersonjr2@gmail.com>
2022-09-24 14:39:57 -0300

    Add exercicio CalculaMedia2

---
+++ src/main/java/anderson/CalculaMedia2.java
@@ -0,0 +1,25 @@
+package anderson;
+
+import java.util.Scanner;
+
+public class CalculaMedia2 {
+    public static void main(String[] args) {
+        Scanner teclado  = new Scanner(System.in);
+        System.out.println("Favor informar o valor : ");
+        var valor = teclado.nextInt();
+        var soma_valor=0;
+        var cont = 0;
+        do {
+            if (valor != -1) {
+                soma_valor = soma_valor + valor;
+                cont++;
+                System.out.println("Favor informar o valor ");
+                valor = teclado.nextInt();
+            }
+        } while (valor != -1);
+        if (cont != 0) {
+            System.out.println("Média = " + soma_valor/cont);
+        }
+
+    }
+}
